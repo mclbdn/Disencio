1940dc8fabf7b4ddb5abb0f16b7974a657b762d4
mclbdn <michalbednar93@gmail.com>
2022-03-21 23:09:39 -0600

    Finish FAQ section

--- components/Faq.js
+++ components/Faq.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { useRef, useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import SingleFaq from "./SingleFaq";
 
 const Section = styled.section`
   position: relative;
@@ -26,35 +25,38 @@ const H2 = styled.h2`
   }
 `;
 
-const H3 = styled.h3`
-  font-family: "Roboto Slab", serif;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 100%;
-  max-width: 70%;
-`;
-
-const SingleFaq = styled.div`
-  display: flex;
-  margin: 0 20px 30px;
-  position: relative;
-  justify-content: space-between;
-  align-items: center;
-
-  &:after {
-    bottom: -15px;
-    left: 0px;
-    position: absolute;
-    content: "";
-    border-bottom: 1px solid;
-    border-color: var(--off-white-color);
-    width: 100%;
-  }
-`;
-
 const Faq = () => {
   const ref = useRef();
   const [isVisible, setIsVisible] = useState(false);
+
+  const faqs = [
+    {
+      faqQuestion: "¿Por qué debo utilizar una empresa de diseño web?",
+      faqAnswer:
+        "Cuando estás considerando crear un sitio web para su negocio, sabemos que se te presentan muchas opciones diferentes. Hay muchas empresas de diseño web, creadores de sitios web como Wix o el amigo de la familia que es bueno con las computadoras. Para algunas personas, las dos últimas opciones pueden ser una opción, pero solo un diseñador web experimentado puede crear un producto que atraiga a los visitantes y los convierta en clientes.",
+    },
+    {
+      faqQuestion: "¿Cómo empiezo?",
+      faqAnswer:
+        "Antes de firmar o comprometerse con algo, tenemos un extenso 'Cuestionario del cliente' que le pedimos que complete para que podamos aprender más sobre sus necesidades, clientes e industria. No hay absolutamente ningún compromiso de su parte al completar este cuestionario.",
+    },
+    {
+      faqQuestion: "¿Cuánto tiempo se tarda en construir mi sitio web?",
+      faqAnswer:
+        "Cada proyecto es diferente, por lo que no hay un cronograma establecido. Sin embargo, tratamos de lanzar nuevos sitios entre dos y tres semanas. Le daremos un cronograma detallado antes de comenzar cualquier trabajo.",
+    },
+    {
+      faqQuestion: "¿Con qué tamaño y tipo de empresas trabajan?",
+      faqAnswer:
+        "Tenemos un equilibrio uniforme de startups, pequeñas empresas y organizaciones sin fines de lucro en una multitud de industrias.",
+    },
+    {
+      faqQuestion:
+        "¿Existirá algún tipo de seguimiento despúes de tener el sitio web?",
+      faqAnswer:
+        "¡Absolutamente! Mantenemos tu sitio web actualizado, seguro y rápido. También ayudamos con mejoras reactivas a su sitio web, como la creación de nuevas páginas de destino o la adición de nuevas funciones.",
+    },
+  ];
 
   useEffect(() => {
     const observerOptions = {
@@ -74,34 +76,18 @@ const Faq = () => {
 
     return () => observer.unobserve(ref.current);
   }, []);
+
   return (
     <Section
       ref={ref}
       style={{ opacity: isVisible ? 1 : 0, transition: "opacity 2.5s" }}
     >
       <H2>FAQ</H2>
-      <SingleFaq>
-        <H3>¿Por qué debo utilizar una empresa de diseño web?</H3>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </SingleFaq>
-      <SingleFaq>
-        <H3>¿Cómo empiezo?</H3>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </SingleFaq>
-      <SingleFaq>
-        <H3>¿Cuánto tiempo se tarda en construir mi sitio web?</H3>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </SingleFaq>
-      <SingleFaq>
-        <H3>¿Con qué tamaño y tipo de empresas trabajan?</H3>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </SingleFaq>
-      <SingleFaq>
-        <H3>
-          ¿Existirá algún tipo de seguimiento despúes de tener el sitio web?
-        </H3>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </SingleFaq>
+      {faqs.map((faq) => {
+        return (
+          <SingleFaq faqQuestion={faq.faqQuestion} faqAnswer={faq.faqAnswer} />
+        );
+      })}
     </Section>
   );
 };
---
+++ components/SingleFaq.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const FaqDiv = styled.div`
+  cursor: pointer;
+  border-bottom: 1px solid var(--off-white-color);
+  padding-bottom: 10px;
+  margin: 0 20px 20px;
+
+  @media (min-width: 740px) {
+    margin: 0 40px 18px;
+    padding-bottom: 18px;
+  }
+
+  @media (min-width: 1440px) {
+    margin: 0 140px 20px;
+    padding-bottom: 20px;
+  }
+`;
+
+const SingleFaqWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const H3 = styled.h3`
+  font-family: "Roboto Slab", serif;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 100%;
+  max-width: 70%;
+
+  @media (min-width: 740px) {
+    font-size: 25px;
+    margin-left: 0px;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: 30px;
+  }
+`;
+
+const Paragraph = styled.p`
+  margin-top: 20px;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 150%;
+`;
+
+const SVGWrapper = styled.div`
+  position: relative;
+  height: 25px;
+  width: 25px;
+
+  @media (min-width: 1440px) {
+    height: 30px;
+    width: 30px;
+  }
+`;
+
+const SingleFaq = ({ faqQuestion, faqAnswer }) => {
+  const [isParagraphVisible, setIsParagraphVisible] = useState(false);
+
+  function toggleFaq() {
+    setIsParagraphVisible(!isParagraphVisible);
+  }
+
+  return (
+    <FaqDiv>
+      <SingleFaqWrapper onClick={toggleFaq}>
+        <H3>{faqQuestion}</H3>
+        {isParagraphVisible ? (
+          <SVGWrapper>
+            <FontAwesomeIcon
+              style={{ position: "absolute", width: "100%", height: "100%" }}
+              icon={faCaretUp}
+            />
+          </SVGWrapper>
+        ) : (
+          <SVGWrapper>
+            <FontAwesomeIcon
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                top: "0px",
+                bottom: "0px",
+                left: "0px",
+                right: "0px",
+              }}
+              icon={faCaretDown}
+            />
+          </SVGWrapper>
+        )}
+      </SingleFaqWrapper>
+      {isParagraphVisible && (
+        <Paragraph onClick={toggleFaq}>{faqAnswer}</Paragraph>
+      )}
+    </FaqDiv>
+  );
+};
+
+export default SingleFaq;
