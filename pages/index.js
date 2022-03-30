e69168ee6b640ae3afa379012ceebd2b177d6c5f
mclbdn <michalbednar93@gmail.com>
2022-03-30 09:24:38 -0600

    change crossorigin for crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
         <title>Diseñcio. | Diseño y Páginas Web Guadalajara</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=Roboto+Slab:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
